764eb2ef0099a80077c17588a3ed8434e6436f05
Aleksei Kuzmin <alkuzmin@microsoft.com>
2018-04-04 15:15:04 +0200

    DevTools: move remote debugging hosted URL to single location.
    https://chromium-review.googlesource.com/905983

--- brightray/browser/devtools_manager_delegate.cc
+++ brightray/browser/devtools_manager_delegate.cc
@@ -87,7 +87,6 @@ CreateSocketFactory() {
 void DevToolsManagerDelegate::StartHttpHandler() {
   content::DevToolsAgentHost::StartRemoteDebuggingServer(
       CreateSocketFactory(),
-      std::string(),
       base::FilePath(),
       base::FilePath());
 }
@@ -117,9 +116,8 @@ std::string DevToolsManagerDelegate::GetDiscoveryPageHTML() {
       IDR_CONTENT_SHELL_DEVTOOLS_DISCOVERY_PAGE).as_string();
 }
 
-std::string DevToolsManagerDelegate::GetFrontendResource(
-    const std::string& path) {
-  return content::DevToolsFrontendHost::GetFrontendResource(path).as_string();
+bool DevToolsManagerDelegate::HasBundledFrontendResources() {
+  return true;
 }
 
 }  // namespace brightray
