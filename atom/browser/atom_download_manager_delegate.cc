a1eaa7248074b4b725e84dec1111a09d2d7e5194
Aleksei Kuzmin <alkuzmin@microsoft.com>
2018-04-10 18:08:50 +0200

    move DownloadInterruptReasons from content/ to components/download
    https://chromium-review.googlesource.com/897967

--- atom/browser/atom_download_manager_delegate.cc
+++ atom/browser/atom_download_manager_delegate.cc
@@ -123,8 +123,8 @@ void AtomDownloadManagerDelegate::OnDownloadPathGenerated(
                download::DownloadItem::TARGET_DISPOSITION_PROMPT,
                download::DOWNLOAD_DANGER_TYPE_NOT_DANGEROUS, path,
                path.empty() ?
-                   content::DOWNLOAD_INTERRUPT_REASON_USER_CANCELED :
-                   content::DOWNLOAD_INTERRUPT_REASON_NONE);
+                   download::DOWNLOAD_INTERRUPT_REASON_USER_CANCELED :
+                   download::DOWNLOAD_INTERRUPT_REASON_NONE);
 }
 
 void AtomDownloadManagerDelegate::Shutdown() {
@@ -142,7 +142,7 @@ bool AtomDownloadManagerDelegate::DetermineDownloadTarget(
                  download::DownloadItem::TARGET_DISPOSITION_OVERWRITE,
                  download::DOWNLOAD_DANGER_TYPE_NOT_DANGEROUS,
                  download->GetForcedFilePath(),
-                 content::DOWNLOAD_INTERRUPT_REASON_NONE);
+                 download::DOWNLOAD_INTERRUPT_REASON_NONE);
     return true;
   }
 
@@ -153,7 +153,7 @@ bool AtomDownloadManagerDelegate::DetermineDownloadTarget(
     callback.Run(save_path,
                  download::DownloadItem::TARGET_DISPOSITION_OVERWRITE,
                  download::DOWNLOAD_DANGER_TYPE_NOT_DANGEROUS,
-                 save_path, content::DOWNLOAD_INTERRUPT_REASON_NONE);
+                 save_path, download::DOWNLOAD_INTERRUPT_REASON_NONE);
     return true;
   }
 
