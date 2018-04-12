2547032e38780757e1c6d8f9e9bf26aaeb9af6c8
deepak1556 <hop2deep@gmail.com>
2018-04-12 19:18:02 +0530

    Remove obsolete ShouldHandleSystemCommands function.
    https://chromium-review.googlesource.com/c/chromium/src/+/924315

--- atom/browser/native_window_views.cc
+++ atom/browser/native_window_views.cc
@@ -1259,10 +1259,6 @@ base::string16 NativeWindowViews::GetWindowTitle() const {
   return base::UTF8ToUTF16(title_);
 }
 
-bool NativeWindowViews::ShouldHandleSystemCommands() const {
-  return true;
-}
-
 views::Widget* NativeWindowViews::GetWidget() {
   return window_.get();
 }
